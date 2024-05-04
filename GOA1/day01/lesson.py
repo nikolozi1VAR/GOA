cccbce5ea0174f240986efb7d22b2836af397378
nikolozi1VAR <m.atcharashvili.ia@gmail.com>
2024-05-04 22:19:59 +0400

    commit

---
+++ GOA1/day01/lesson.py
@@ -0,0 +1,22 @@
+name = "nikoloz"
+# name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებლი სიმბოლო
+# "nikoloz" არის ცვლადისთვის მნიშვნელობა
+surname = "tsukoshvili"
+# print ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+# name = "nikoloz" ეს არის str(string) ტიპის ცვლადი
+age = 10 # ეს არის int (integer) მთელი რიცხვი
+height = 1.50 # ეს არის float ტიპის ცვლადი(ათწილადები)
+knows_programming = True
+#print(name + " " + surname)
+# string არის ბრჭყალებში მოქცეული სიმბოლოები
+
+# print(type(age)) # age გადეცა type ფუნქციას რომელმაც დააბრუნა მისი ტიპი
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+print(name + " " +surname)
